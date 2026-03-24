d696005dc76c1b79866dc61260b3f4f4c1a5cd26
Abdelrahman Wael Ammar <73370043+bodiwael@users.noreply.github.com>
2026-03-24 02:28:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       '/sheets-api': {
         target: 'https://script.google.com',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/sheets-api/, '/macros/s/AKfycbwGNyERLjQN_xDphUDsaem87-nT2hG98RT6boXnesgrabKSkLqRB4VVQZYdlC8FNI8/exec'),
+        rewrite: (path) => path.replace(/^\/sheets-api/, '/macros/s/AKfycbxMR6-OHYVLwfBXUhYkBcj7ZInHsRX83Yf1s6aP-_4Jyoo-DT8J_oBRqr_DVl-srxM4/exec'),
         secure: true,
       }
     }
